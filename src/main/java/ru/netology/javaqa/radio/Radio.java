e647404d2cc90d7cac0761f09050c2b43b316694
Pavel Shvydkin <Pshvydkin@gmail.com>
2023-05-11 21:37:42 +0300

    modified Radio.java RadioTest.Java

--- src/main/java/ru/netology/javaqa/radio/Radio.java
+++ src/main/java/ru/netology/javaqa/radio/Radio.java
@@ -1,75 +1,75 @@
 package ru.netology.javaqa.radio;
 
-// объявление публичного класс 'Radio'
 public class Radio {
-    // объявление непубличного целочисленного поуля 'maxNumberStation' (максимальное количество станиций)
-    private final int maxNumberStation;
-    // объявление  непубличного целочисленного поля 'currentNumberStation' (текущий номер радиостанции)
-    private int currentNumberStation;
-    // объявление  непубличного целочисленного поля 'currentVolumeStation' (текущая громкость радиостанции)
-    private int currentVolumeStation;
-    // объявление конструктора 'Radio' без параметров
+    private final int numStations;
+    private int currentStation;
+    private int volume;
+
     public Radio() {
-        this.maxNumberStation = 10;
-        this.currentNumberStation = 0;
-        this.currentVolumeStation = 0;
+        this.numStations = 10;
+        this.currentStation = 0;
+        this.volume = 0;
     }
-    // объявление конструктора 'Radio' c  параметром
-    public Radio(int maxNumberStation) {
-        this.maxNumberStation = maxNumberStation;
-        this.currentNumberStation = currentNumberStation;
-        this.currentVolumeStation = currentVolumeStation;
-    }
-    // получаем текущее значение поля 'maxNumberStation' из объектов класса 'Radio' используя метод 'getMaxNumberStation'
-    public int getMaxNumberStation() {
-        return maxNumberStation;
-    }
-    // получаем текущее значение поля 'currentNumberStation' из объектов класа 'Radio' используя метод 'getCurrentNumberStation'
-    public int getCurrentNumberStation() {
-        return currentNumberStation;
-    }
-    // изменяем текущее значение поля 'currentNumberStation' из объектов класса 'Radio' используя метод 'setCurrentNumberStation'
-    public void setCurrentNumberStation(int newCurrentNumberStation) {
-        if (newCurrentNumberStation < 0 || newCurrentNumberStation >= maxNumberStation) {
-            return;
-        }
-        currentNumberStation = newCurrentNumberStation;
-    }
-    // получаем текущее значение поля 'currentVolumeStation' из объектов класа 'Radio' используя метод 'getCurrentVolumeStation'
-    public int getCurrentVolumeStation() {
-        return currentVolumeStation;
-    } // конец метода 'getCurrentVolumeStation"
-    // изменяем текущее значение поля 'CurrentVolumeStation' из объектов класса 'Radio' используя метод 'setCurrentVolumeStation'
-    public void setCurrentVolumeStation(int newCurrentVolumeStation) {
-        if (newCurrentVolumeStation < 0 || newCurrentVolumeStation > 100) {
-            return;
-        }
-        currentVolumeStation = newCurrentVolumeStation;
-    } // конец метода 'setCurrentVolumeStation'
 
-    public void nextStation() { // объявление публичного метода 'nextStation'
-        if (currentNumberStation == maxNumberStation -1) {
-            currentNumberStation = 0;
-        } else {
-            currentNumberStation ++;
-        }
-    } // конец объявления метода 'nextStation'
-    public void prevStation() { // объявление публичного метода 'prevStation'
-        if (currentNumberStation == 0) {
-            currentNumberStation = maxNumberStation -1;
-        } else {
-            currentNumberStation --;
-        }
-    } // конец объявления метода 'prevStation'
-    public void increaseVolumeStation() { // объявление публичного метода 'increaseVolumeStation'
-        if (currentVolumeStation < 100) {
-            currentVolumeStation ++;
-        }
-    }  // конец объявления метода 'increaseVolumeStation'
-    public void decreaseVolumeStation() { // объявление публичного метода 'decreaseVolumeStation'
-        if (currentVolumeStation > 0) {
-            currentVolumeStation --;
-        }
-    } // конец объявления метода 'decreaseVolumeStation'
+    public Radio(int numStations) {
+        this.numStations = numStations;
+        this.currentStation = 0;
+        this.volume = 0;
+    }
 
-} // конец объявления класса Radio
+    public int getNumStations() {
+        return numStations;
+    }
+
+    public int getCurrentStation() {
+        return currentStation;
+    }
+
+    public void setCurrentStation(int station) {
+        if (station >= 0 && station < numStations) {
+            this.currentStation = station;
+        }
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setCurrentVolume(int volume) {
+        if (volume >= 0 && volume <= 100) {
+            this.volume = volume;
+        }
+    }
+
+    public void setStation(int station) {
+        if (station >= 0 && station < numStations) {
+            this.currentStation = station;
+        }
+    }
+
+    public void nextStation() {
+        currentStation++;
+        if (currentStation >= numStations) {
+            currentStation = 0;
+        }
+    }
+
+    public void prevStation() {
+        currentStation--;
+        if (currentStation < 0) {
+            currentStation = numStations - 1;
+        }
+    }
+
+    public void increaseVolume() {
+        if (volume < 100) {
+            volume++;
+        }
+    }
+
+    public void decreaseVolume() {
+        if (volume > 0) {
+            volume--;
+        }
+    }
+}
--- src/test/java/ru/netology/javaqa/radio/RadioTest.java
+++ src/test/java/ru/netology/javaqa/radio/RadioTest.java
@@ -3,128 +3,122 @@ package ru.netology.javaqa.radio;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class RadioTest { // объявление публичного тест класса 'RadioTest'
+public class RadioTest {
+
     Radio radio = new Radio();
-    // 1. положительный тест, проверяет установку текущей радиостанции на определенный номер
-    @Test
-    public void shouldSetCurrentStation() {
-        radio.setCurrentNumberStation(5);
 
-        Assertions.assertEquals(5, radio.getCurrentNumberStation());
+    @Test
+    public void testConstructorWithoutParameter() {
+        Assertions.assertEquals(10, radio.getNumStations());
+        Assertions.assertEquals(0, radio.getCurrentStation());
+        Assertions.assertEquals(0, radio.getVolume());
     }
-    // 2. положительный тест, проверяет увеличение громкости на единицу.
+
     @Test
-    public void shouldIncreaseVolume() {
-
-        radio.increaseVolumeStation();
-
-        Assertions.assertEquals(1, radio.getCurrentVolumeStation());
+    public void testConstructorWithParameter() {
+        Radio customRadio = new Radio(20);
+        Assertions.assertEquals(20, customRadio.getNumStations());
+        Assertions.assertEquals(0, customRadio.getCurrentStation());
+        Assertions.assertEquals(0, customRadio.getVolume());
     }
-    // 3. отрицательный тест, проверяет, что нельзя установить громкость больше 100.
-    @Test
-    public void shouldNotSetVolumeMoreThanHundred() {
-        radio.setCurrentVolumeStation(101);
 
-        Assertions.assertEquals(0, radio.getCurrentVolumeStation());
-    }
-    // 4. положительный тест, проверяет уменьшение громкости на единицу.
     @Test
-    public void shouldDecreaseVolume() {
-        radio.setCurrentVolumeStation(50);
-        radio.decreaseVolumeStation();
+    public void testGetNumStations() {
+        Assertions.assertEquals(10, radio.getNumStations());
+    }
 
-        Assertions.assertEquals(49, radio.getCurrentVolumeStation());
-    }
-    // 5. положительный тест, проверяет переключение на следующую радиостанцию.
     @Test
-    public void shouldSwitchNextNumberStation() {
-        radio.setCurrentNumberStation(8);
+    public void testGetCurrentStation() {
+        Assertions.assertEquals(0, radio.getCurrentStation());
+    }
+
+    @Test
+    public void testSetCurrentStation_ValidStation() {
+        radio.setCurrentStation(5);
+        Assertions.assertEquals(5, radio.getCurrentStation());
+    }
+
+    @Test
+    public void testSetCurrentStation_InvalidStation() {
+        radio.setCurrentStation(15);
+        Assertions.assertEquals(0, radio.getCurrentStation());
+        radio.setCurrentStation(-5);
+        Assertions.assertEquals(0, radio.getCurrentStation());
+    }
+
+    @Test
+    public void testGetVolume() {
+        Assertions.assertEquals(0, radio.getVolume());
+    }
+
+    @Test
+    public void testSetCurrentVolume_ValidVolume() {
+        radio.setCurrentVolume(50);
+        Assertions.assertEquals(50, radio.getVolume());
+    }
+
+    @Test
+    public void testSetCurrentVolume_InvalidVolume() {
+        radio.setCurrentVolume(120);
+        Assertions.assertEquals(0, radio.getVolume());
+        radio.setCurrentVolume(-10);
+        Assertions.assertEquals(0, radio.getVolume());
+    }
+
+    @Test
+    public void testSetStation_ValidStation() {
+        radio.setStation(7);
+        Assertions.assertEquals(7, radio.getCurrentStation());
+    }
+
+    @Test
+    public void testSetStation_InvalidStation() {
+        radio.setStation(15);
+        Assertions.assertEquals(0, radio.getCurrentStation());
+        radio.setStation(-5);
+        Assertions.assertEquals(0, radio.getCurrentStation());
+    }
+
+    @Test
+    public void testNextStation() {
+        radio.setStation(8);
         radio.nextStation();
-
-        Assertions.assertEquals(9, radio.getCurrentNumberStation());
-    }
-    // 6. положительный тест, проверяет переключение на предыдущую радиостанцию.
-    @Test
-    public void shouldSwitchPrevNumberStation() {
-        radio.setCurrentNumberStation(5);
-        radio.prevStation();
-
-        Assertions.assertEquals(4, radio.getCurrentNumberStation());
-    }
-    // 7. положительный тест, проверяет переключение с последней радиостанции на первую.
-    @Test
-    public void shouldSwitchToFirstNumberStationThanOnLastNumberStation() {
-        radio.setCurrentNumberStation(9);
+        Assertions.assertEquals(9, radio.getCurrentStation());
         radio.nextStation();
-
-        Assertions.assertEquals(0, radio.getCurrentNumberStation());
+        Assertions.assertEquals(0, radio.getCurrentStation());
     }
-    // 8. положительный тест, проверяет переключение с первой радиостанции на последнюю.
+
     @Test
-    public void shouldSwitchToLastNumberStationWhenOnFirstNumberStation() {
-        radio.setCurrentNumberStation(0);
+    public void testPrevStation() {
+        radio.setStation(1);
         radio.prevStation();
-
-        Assertions.assertEquals(9, radio.getCurrentNumberStation());
+        Assertions.assertEquals(0, radio.getCurrentStation());
+        radio.prevStation();
+        Assertions.assertEquals(9, radio.getCurrentStation());
     }
-    // 9. Положительный тест, проверяет выставленное значение максимального количества станций
+
     @Test
-    public void shouldSetMaxNumberStation(){
-
-        Assertions.assertEquals(10, radio.getMaxNumberStation());
+    public void testIncreaseVolume() {
+        radio.increaseVolume();
+        Assertions.assertEquals(1, radio.getVolume());
+        radio.setCurrentVolume(99);
+        radio.increaseVolume();
+        Assertions.assertEquals(100, radio.getVolume());
+        radio.increaseVolume();
+        Assertions.assertEquals(100, radio.getVolume());
     }
-    // 10. Положительный тест, проверяет создание объекта с заданным параметром поля максимальное количество станций и корректным выставлением значений полей текущий номер станции и текущая громкость
+
     @Test
-    public void ConstructorWithParameterMaxNumberStationTest(){
-        Radio radio = new Radio(8);
-
-        Assertions.assertEquals(8, radio.getMaxNumberStation());
-        Assertions.assertEquals(0, radio.getCurrentNumberStation());
-        Assertions.assertEquals(0, radio.getCurrentVolumeStation());
+    public void testDecreaseVolume() {
+        radio.decreaseVolume();
+        Assertions.assertEquals(0, radio.getVolume());
+        radio.setCurrentVolume(50);
+        radio.decreaseVolume();
+        Assertions.assertEquals(49, radio.getVolume());
+        radio.decreaseVolume();
+        Assertions.assertEquals(48, radio.getVolume());
+        radio.setCurrentVolume(0);
+        radio.decreaseVolume();
+        Assertions.assertEquals(0, radio.getVolume());
     }
-    // 11. Положительный тест, проверяет создание объекта без параметара с выставленным по умолчанию значением поля максимальное количество станций и корректным выставлением значений полей текущий номер станции и текущая громкость
-    @Test
-    public void ConstructorWithoutParameterMaxNumberStationTest(){
-        Radio radio = new Radio();
-
-        Assertions.assertEquals(10, radio.getMaxNumberStation());
-        Assertions.assertEquals(0, radio.getCurrentNumberStation());
-        Assertions.assertEquals(0, radio.getCurrentVolumeStation());
-    }
-    // 12. отрицательный тест, проверяет, что нельзя установить текущую радиостанцию с номером меньше нуля.
-    @Test
-    public void shouldNotSetCurrentStationLessThanZero() {
-        radio.setCurrentNumberStation(-1);
-
-        Assertions.assertEquals(0, radio.getCurrentNumberStation());
-    }
-    // 13.  отрицательный тест, проверяет, что нельзя установить текущую радиостанцию с номером больше 9.
-    @Test
-    public void shouldNotCurrentStationMoreThanNine() {
-        radio.setCurrentNumberStation(10);
-
-        Assertions.assertEquals(0, radio.getCurrentNumberStation());
-    }
-    // 14. отрицательный тест, проверяет, что нельзя увеличить громкость больше 100.
-    @Test
-    public void shouldNotIncreaseVolumeMoreThanHundred() {
-        radio.setCurrentVolumeStation(100);
-        radio.increaseVolumeStation();
-
-        Assertions.assertEquals(100, radio.getCurrentVolumeStation());
-    }
-    // 15. отрицательный тест, проверяет, что нельзя установить громкость меньше нуля.
-    @Test
-    public void shouldNotSetVolumeLessThanZero() {
-        radio.setCurrentVolumeStation(-1);
-
-        Assertions.assertEquals(0, radio.getCurrentVolumeStation());
-    }
-    // 16. отрицательный тест, проверяет, что нельзя уменьшить громкость меньше нуля.
-    @Test
-    public void shouldNotDecreaseVolumeLessThanZero() {
-        radio.decreaseVolumeStation();
-
-        Assertions.assertEquals(0, radio.getCurrentVolumeStation());
-    }
-} // конец объявления класса 'RadioTest'
+}
